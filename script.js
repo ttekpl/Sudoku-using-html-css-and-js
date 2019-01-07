6c7fd17a8788d5bd41f8b88c44d9871fb09adabb
ttekpl <dkakol01@gamail.com>
2019-01-07 17:22:49 +0100

    X checking

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  let isNumbersDuplicate = false;
  let inputsValues = [];
  let isNumbersOnly = true;
- const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 
  function validate() {
      isNumbersDuplicate = false;
@@ -27,12 +27,13 @@
 
      if (isNumbersOnly == false) {
          window.alert(
-             "W niektórcych miejscach znajdują się niepoprawne znaki lub nie wypełniłeś wszystkich pól!"
+             "You entered invalid type of values or didn't fill evey gap!"
          );
          return;
      }
 
      function checkY() {
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
          let whichColumn = 0;
 
 
@@ -81,7 +82,7 @@
                  if (is > 1) {
 
                      window.alert(
-                         "Liczby się powtarzają");
+                         "Numbers are repeating");
                      isNumbersDuplicate = true;
                      return;
                  }
@@ -103,7 +104,84 @@
      }
 
 
-     checkY();
 
+
+     function checkX() {
+         console.log("weszło");
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+
+         for (let i = 0; i < 10; i++) {
+             for (let j = 0; j < 10; j++) {
+                 console.log("weszło 2 a index to ");
+                 console.log(j * 10 + i);
+                 switch (inputsValues[j * 10 + i]) {
+                     case "0":
+                         isDuplicate[0]++;
+                         break;
+
+                     case "1":
+                         isDuplicate[1]++;
+                         break;
+                     case "2":
+                         isDuplicate[2]++;
+                         break;
+                     case "3":
+                         isDuplicate[3]++;
+                         break;
+                     case "4":
+                         isDuplicate[4]++;
+                         break;
+                     case "5":
+                         isDuplicate[5]++;
+                         break;
+                     case "6":
+                         isDuplicate[6]++;
+                         break;
+                     case "7":
+                         isDuplicate[7]++;
+                         break;
+                     case "8":
+                         isDuplicate[8]++;
+                         break;
+                     case "9":
+                         isDuplicate[9]++;
+                         break;
+
+
+                 }
+
+             }
+             for (const is of isDuplicate) {
+
+                 if (is > 1) {
+
+                     window.alert(
+                         "Numbers are repeating X");
+                     isNumbersDuplicate = true;
+                     return;
+                 }
+
+
+             }
+             for (let i = 0; i < 10; i++) {
+                 isDuplicate[i] = 0;
+             }
+
+
+
+
+
+         }
+
+
+
+     }
+
+     checkY();
+     if (isNumbersDuplicate) return;
+     checkX();
+     if (isNumbersDuplicate) return;
 
  }
