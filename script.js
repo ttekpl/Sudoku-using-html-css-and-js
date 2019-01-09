da3986f6a1e7c38964da79ad110f46a22b642e6c
ttekpl <dkakol01@gamail.com>
2019-01-09 22:04:47 +0100

    repair

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
      inputsValues.forEach(function (value, index) {
 
-         if (((isNaN(value * 1) == true) || (value == "")) || (value == " ")) {
+         if (((isNaN(value * 1) == true) || (value == "")) || (value == " ") || (value == "0")) {
 
              isNumbersOnly = false;
          }
@@ -33,17 +33,14 @@
      }
 
      function checkY() {
-         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, ];
          let whichColumn = 0;
 
 
 
-         for (let i = 0; i < 10; i++) {
-             for (let j = 0; j < 10; j++) {
+         for (let i = 0; i < 9; i++) {
+             for (let j = 0; j < 9; j++) {
                  switch (inputsValues[j + whichColumn]) {
-                     case "0":
-                         isDuplicate[0]++;
-                         break;
 
                      case "1":
                          isDuplicate[1]++;
@@ -89,7 +86,7 @@
 
 
              }
-             for (let i = 0; i < 10; i++) {
+             for (let i = 0; i < 9; i++) {
                  isDuplicate[i] = 0;
              }
 
@@ -108,18 +105,15 @@
 
      function checkX() {
          console.log("weszło");
-         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
 
-         for (let i = 0; i < 10; i++) {
-             for (let j = 0; j < 10; j++) {
+         for (let i = 0; i < 9; i++) {
+             for (let j = 0; j < 9; j++) {
                  console.log("weszło 2 a index to ");
                  console.log(j * 10 + i);
                  switch (inputsValues[j * 10 + i]) {
-                     case "0":
-                         isDuplicate[0]++;
-                         break;
 
                      case "1":
                          isDuplicate[1]++;
@@ -165,7 +159,7 @@
 
 
              }
-             for (let i = 0; i < 10; i++) {
+             for (let i = 0; i < 9; i++) {
                  isDuplicate[i] = 0;
              }
 
@@ -179,6 +173,78 @@
 
      }
 
+     function checkSquare() {
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+         let whichColumn = 0;
+
+
+
+         for (let i = 0; i < 3; i++) {
+             for (let j = 0; j < 3; j++) {
+                 for (let k = 0; k < 9; k++) {
+                     switch (inputsValues[j + whichColumn]) {
+                         case "0":
+                             isDuplicate[0]++;
+                             break;
+
+                         case "1":
+                             isDuplicate[1]++;
+                             break;
+                         case "2":
+                             isDuplicate[2]++;
+                             break;
+                         case "3":
+                             isDuplicate[3]++;
+                             break;
+                         case "4":
+                             isDuplicate[4]++;
+                             break;
+                         case "5":
+                             isDuplicate[5]++;
+                             break;
+                         case "6":
+                             isDuplicate[6]++;
+                             break;
+                         case "7":
+                             isDuplicate[7]++;
+                             break;
+                         case "8":
+                             isDuplicate[8]++;
+                             break;
+                         case "9":
+                             isDuplicate[9]++;
+                             break;
+
+
+                     }
+                 }
+
+             }
+             for (const is of isDuplicate) {
+
+                 if (is > 1) {
+
+                     window.alert(
+                         "Numbers are repeating");
+                     isNumbersDuplicate = true;
+                     return;
+                 }
+
+
+             }
+             for (let i = 0; i < 10; i++) {
+                 isDuplicate[i] = 0;
+             }
+
+             whichColumn += 10;
+
+
+
+         }
+
+
+
+     }
      checkY();
      if (isNumbersDuplicate) return;
      checkX();
