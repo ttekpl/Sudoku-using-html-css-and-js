4659641d1c3181ccf554d0fdb3abb651253da4a7
ttekpl <dkakol01@gamail.com>
2019-01-09 22:47:18 +0100

    doing square

--- script.js
+++ script.js
@@ -8,6 +8,15 @@
      isNumbersOnly = true;
      inputsValues = [];
      const inputsArray = document.querySelectorAll('input');
+     const first = document.querySelectorAll(".first");
+     const second = document.querySelectorAll(".second");
+     const third = document.querySelectorAll(".third");
+     const fourth = document.querySelectorAll(".fourth");
+     const fifth = document.querySelectorAll(".fifth");
+     const sixth = document.querySelectorAll(".sixth");
+     const seventh = document.querySelectorAll(".seventh");
+     const eigth = document.querySelectorAll(".eigth");
+     const nineth = document.querySelectorAll(".nineth");
 
 
      inputsArray.forEach(function (input, index) {
@@ -31,6 +40,56 @@
          );
          return;
      }
+
+     const firsta = first.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+
+     });
+     console.log(firsta);
+
+     const seconda = second.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const thirda = third.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+
+     const fourtha = fourth.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const fiftha = fifth.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const sixtha = sixth.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const seventha = seventh.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const eigtha = eigth.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
+     const ninetha = nineth.forEach(function (input, index) {
+         let a = [];
+         a[index] = input.value;
+         return a;
+     });
 
      function checkY() {
          const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0, ];
@@ -100,9 +159,6 @@
 
      }
 
-
-
-
      function checkX() {
          console.log("weszło");
          const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -164,84 +220,81 @@
              }
 
 
+         }
 
+     }
+
+
+
+     function squareOneCheck(str) {
+         console.log("weszło");
+         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
+         str.forEach((f) => {
+             switch (f) {
+                 case "1":
+                     isDuplicate[0]++;
+                     break;
+                 case "2":
+                     isDuplicate[1]++;
+                     break;
+                 case "3":
+                     isDuplicate[2]++;
+                     break;
+                 case "4":
+                     isDuplicate[3]++;
+                     break;
+                 case "5":
+                     isDuplicate[4]++;
+                     break;
+                 case "6":
+                     isDuplicate[5]++;
+                     break;
+                 case "7":
+                     isDuplicate[6]++;
+                     break;
+                 case "8":
+                     isDuplicate[7]++;
+                     break;
+                 case "9":
+                     isDuplicate[8]++;
+                     break;
+             }
+
+
+         })
+
+
+         for (const is of isDuplicate) {
+             console.log(is);
+             if (is > 1) {
+
+                 window.alert(
+                     "Numbers are repeating square");
+                 isNumbersDuplicate = true;
+                 return;
+             }
 
 
          }
-
-
-
      }
 
      function checkSquare() {
-         const isDuplicate = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-         let whichColumn = 0;
-
-
-
-         for (let i = 0; i < 3; i++) {
-             for (let j = 0; j < 3; j++) {
-                 for (let k = 0; k < 9; k++) {
-                     switch (inputsValues[j + whichColumn]) {
-
-                         case "1":
-                             isDuplicate[0]++;
-                             break;
-                         case "2":
-                             isDuplicate[1]++;
-                             break;
-                         case "3":
-                             isDuplicate[2]++;
-                             break;
-                         case "4":
-                             isDuplicate[3]++;
-                             break;
-                         case "5":
-                             isDuplicate[4]++;
-                             break;
-                         case "6":
-                             isDuplicate[5]++;
-                             break;
-                         case "7":
-                             isDuplicate[6]++;
-                             break;
-                         case "8":
-                             isDuplicate[7]++;
-                             break;
-                         case "9":
-                             isDuplicate[8]++;
-                             break;
-
-
-                     }
-                 }
-
-             }
-             for (const is of isDuplicate) {
-
-                 if (is > 1) {
-
-                     window.alert(
-                         "Numbers are repeating");
-                     isNumbersDuplicate = true;
-                     return;
-                 }
-
-
-             }
-             for (let i = 0; i < 9; i++) {
-                 isDuplicate[i] = 0;
-             }
-
-             whichColumn += 10;
-
-
-
-         }
-
+         squareOneCheck(firsta);
+         squareOneCheck(seconda);
+         squareOneCheck(thirda);
+         squareOneCheck(fourtha);
+         squareOneCheck(fiftha);
+         squareOneCheck(sixtha);
+         squareOneCheck(seventha);
+         squareOneCheck(eigtha);
+         squareOneCheck(ninetha);
 
 
      }
+     checkSquare();
+     if (isNumbersDuplicate) return;
      checkY();
      if (isNumbersDuplicate) return;
      checkX();
